b8905da105c66c526867101032ee72be973be898
MrunaliDeore <mrunalideoreohk1@gmail.com>
2022-07-13 15:03:47 +0530

    Refactor: calculate working hrs or wage for month

--- empWageComputation.java
+++ empWageComputation.java
@@ -6,15 +6,16 @@ public class empWageComputation
 	public static  final int FULL_TIME=2;
 	public static  final int WAGE_PER_HOUR=20;
 	public static final int WORK_DAY=2;
+	public static final int MAX_HRS_MONTH=10;
 
 	public static void main (String[] args)
 	{		
-		int emphrs=0,empwage=0,totalwage=0;;
-		
-		for(int day=0;day<WORK_DAY;day ++)
-		{
+		int emphrs=0,empwage=0,totalworkingdays=0,totalemphrs=0;
 
-			int empCheck=(int) Math.floor(Math.random()*10%3);
+		while(totalemphrs<=MAX_HRS_MONTH && totalworkingdays<WORK_DAY)
+		{
+			totalworkingdays ++;
+			int empCheck= (int) Math.floor(Math.random()*10%3);
 			switch(empCheck)
 			{
 			case PART_TIME:
@@ -26,13 +27,12 @@ public class empWageComputation
 				break;
 
 			default:
-				emphrs=0;
-
+				emphrs=0;					
 			}
-		}	
-		empwage=emphrs*WAGE_PER_HOUR;
-		totalwage +=empwage;
-
-		System.out.println("Total Employee Wage :"+totalwage);
+			totalemphrs +=emphrs;		
+			System.out.println("Days :"+totalworkingdays + "\nEmp Hrs :"+ emphrs);
+		}		
+		int totalempwage= totalemphrs * WAGE_PER_HOUR;
+		System.out.println("Total Employee wage :"+totalempwage);
 	}
 }
