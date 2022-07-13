432f58bec48d958b5096a2155955840177536143
MrunaliDeore <mrunalideoreohk1@gmail.com>
2022-07-13 14:30:37 +0530

    Refactor: calculate daily emp wage

--- empWageComputation.java
+++ empWageComputation.java
@@ -3,14 +3,19 @@ package Eight;
 public class empWageComputation
 {
 	public static  final int FULL_TIME=1;
-	
+	public static final int WAGE_PER_HOUR=20;
+
 	public static void main (String[] args)
 	{		
+		int emphrs=0,empwage=0;
 		int empCheck=(int) Math.floor(Math.random()*10%2);
 
 		if(empCheck==FULL_TIME)
-			System.out.println("Employee is present");
+			emphrs=8;
 		else
-			System.out.println("Employee is not present");
+			emphrs=0;
+		empwage=emphrs*WAGE_PER_HOUR;
+
+		System.out.println("Employee Wage :"+empwage);
 	}
 }
