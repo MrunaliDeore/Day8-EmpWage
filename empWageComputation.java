9e2f605d676f83ed6da1824712a52558a2c12a4e
MrunaliDeore <mrunalideoreohk1@gmail.com>
2022-07-13 14:49:55 +0530

    Refactor: Part time emp & wage

--- empWageComputation.java
+++ empWageComputation.java
@@ -2,18 +2,22 @@ package Eight;
 
 public class empWageComputation
 {
-	public static  final int FULL_TIME=1;
+	public static  final int PART_TIME=1;
+	public static  final int FULL_TIME=2;
 	public static  final int WAGE_PER_HOUR=20;
 
 	public static void main (String[] args)
 	{		
 		int emphrs=0,empwage=0;
-		int empCheck=(int) Math.floor(Math.random()*10%2);
+		int empCheck=(int) Math.floor(Math.random()*10%3);
 
-		if(empCheck==FULL_TIME)
+		if(empCheck==PART_TIME)
+			emphrs=4;
+		else if(empCheck==FULL_TIME)
 			emphrs=8;
 		else
 			emphrs=0;
+
 		empwage=emphrs*WAGE_PER_HOUR;
 
 		System.out.println("Employee Wage :"+empwage);
